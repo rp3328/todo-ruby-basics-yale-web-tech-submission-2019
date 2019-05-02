6b4b53a8f778a83cecc4737601b042ac19458e1f
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-02 22:53:13 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -15,11 +15,11 @@ def greeting(greeting, name)
 end
 
 def return_a_value(phrase = "Nice")
-  puts "Nice"
+  sum = "Nice"
 end
 
 def last_evaluated_value
-  puts "expert"
+  out = "expert"
 end
 
 def pizza_party(input = "cheese")
